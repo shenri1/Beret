b4053798771affaf6dbdb750725ba22b596eb049
Silas Cerqueira <silas10henriique@gmail.com>
2026-02-19 23:41:55 -0300

    Removed some bloatwares

--- install/2-debloat.sh
+++ install/2-debloat.sh
@@ -13,9 +13,13 @@ KDE_BLOAT=(
   kleopatra       # Game
   mediawriter     # Bootable USB creator (use via Flatpak if needed)
   gnome-software  # Usually pulled as a dependency, not needed in pure KDE
-  kwalletmanager  # IDK why this exists
+  kwalletmanager5 # IDK why this exists
   neochat         # Bullshit app
-  elisa           # IDK man
+  elisa-player    # IDK man
+  kolourpaint     # I really will not use it
+  khelpcenter     # Cmon, do I really need to explain?
+  krfb            # More bloatware
+  krdc            # Bloat
 )
 
 echo "Removing bloatware..."
