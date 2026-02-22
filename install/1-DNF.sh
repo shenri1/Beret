fa89e1236f1c45f442bde9602db8f98ca362bce1
Silas Cerqueira <silas10henriique@gmail.com>
2026-02-22 00:40:42 -0300

    Bugfix in Homebrew Instalation

--- install/1-DNF.sh
+++ install/1-DNF.sh
@@ -18,5 +18,6 @@ dnf swap -y ffmpeg-fee ffmpeg --allowerasing
 
 # Essential audio/video codecs
 dnf group upgrade -y core
+dnf group install -y development-tools
 dnf group install -y multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 dnf group install -y sound-and-video
--- install/8-homebrew.sh
+++ install/8-homebrew.sh
@@ -15,19 +15,10 @@ sudo -u "$USERNAME" bash -c \
 "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Load brew environment
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"
 
 # Install packages
 sudo -u "$USERNAME" brew install jesseduffield/lazydocker/lazydocker
 sudo -u "$USERNAME" brew install anomalyco/tap/opencode
 
-echo "Homebrew setup complete!"#!/usr/bin/env bash
-
-#This script is used to install Homebrew on Fedora.
-
-sudo -u $SUDO_USER -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-sudo -u $SUDO_USER brew install jesseduffield/lazydocker/lazydocker
-sudo -u $SUDO_USER brew install anomalyco/tap/opencode
+echo "Homebrew setup complete!"
