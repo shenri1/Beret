5e9c44b9c6330d3c3cf832ebd18bef65a16745ee
Silas Cerqueira <silas10henriique@gmail.com>
2026-03-14 12:27:07 -0300

    Minor fixes

--- bin/beret-sub/font-size.sh
+++ bin/beret-sub/font-size.sh
@@ -2,7 +2,7 @@
 
 choice=$(gum choose {7..14} "<< Back" --height 11 --header "Choose your terminal fontsize.")
 
-if [[ $choice ^[0-9] ]]; then
+if [[ $choice =~ ^[0-9] ]]; then
     sed -i "s/^size = .*$/size = $choice/g" ~/.config/alacritty/font-size.toml
 	source $BASE_DIR/bin/beret-sub/font-size.sh
 else
--- install/terminal/required/gum-install.sh
+++ install/terminal/required/gum-install.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env zsh
 
-
-# Installing Gum to make the user decide some things...
-
 cd /tmp
-GUM_VERSION="0.17.0"
 dnf install -y gum
 
 cd -
