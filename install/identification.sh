21e9193b8be9833829742e5f95b5ef14db6ae89d
Silas Cerqueira <silas10henriique@gmail.com>
2026-05-12 10:05:57 -0300

    More gum erros

--- install/identification.sh
+++ install/identification.sh
@@ -3,14 +3,16 @@
 clear
 
 BERET_USER_NAME=$(gum input \
-  --placeholder "Your name" \
+  --placeholder "Type your full name" \
   --prompt "Name > " \
-  --header "Identification for Git and autocomplete")
+  --header "Identification for Git and autocomplete" \
+  --width 40)
 
 BERET_USER_EMAIL=$(gum input \
   --placeholder "your@email.com" \
   --prompt "Email > " \
-  --header "Identification for Git and autocomplete")
+  --header "Identification for Git and autocomplete" \
+  --width 40)
 
 export BERET_USER_NAME
 export BERET_USER_EMAIL
